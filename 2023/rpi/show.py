e459a20643671489ea54b982ef44b2e746eb3104
Laco Papay <lacop@lacop.net>
2023-12-01 19:10:41 +0100

    better transitions by shuffling

--- 2023/rpi/show.py
+++ 2023/rpi/show.py
@@ -1,3 +1,5 @@
+import random
+
 def rgbloop(speed, cycles, intensity):
     colors = [
         [
@@ -23,14 +25,26 @@ def chase(speed, cycles, color, length):
 SCENES = [
     ('rgbloop', lambda: rgbloop(speed=0.1, cycles=10, intensity=32)),
     ('chase', lambda: chase(speed=0.1, cycles=10, color=(32, 0, 0), length=10)),
+    ('chase', lambda: chase(speed=0.1, cycles=10, color=(0, 32, 0), length=10)),
+    ('chase', lambda: chase(speed=0.1, cycles=10, color=(0, 0, 32), length=10)),
 ]
 
 def generator():
     scene = 0
+    scene_queue = []
     while True:
+        if len(scene_queue) == 0:
+            scene_queue = list(range(len(SCENES)))
+            random.shuffle(scene_queue)
+            # Make it seem more random by making sure next scene is always different.
+            if scene_queue[0] == scene:
+                scene_queue[0], scene_queue[-1] = scene_queue[-1], scene_queue[0]
+        scene = scene_queue.pop(0)
+
+        # Yield frames from the scene generator until it finishes.
         name = SCENES[scene][0]
         generator = SCENES[scene][1]()
         for (colors, delay) in generator:
             yield (name, colors, delay)
-        scene = (scene + 1) % len(SCENES)
+        
         # TODO fade in/out?
